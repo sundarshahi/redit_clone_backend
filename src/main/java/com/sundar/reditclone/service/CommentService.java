592220cfdf78d489c89703d89e3640480946793b
sundarshahi <shahithakurisundar@gmail.com>
2020-05-22 13:39:44 +0545

    Comments Api

---
+++ src/main/java/com/sundar/reditclone/service/CommentService.java
@@ -0,0 +1,62 @@
+package com.sundar.reditclone.service;
+
+import com.sundar.reditclone.dto.CommentsDto;
+import com.sundar.reditclone.exceptions.PostNotFoundException;
+import com.sundar.reditclone.mapper.CommentMapper;
+import com.sundar.reditclone.model.Comment;
+import com.sundar.reditclone.model.NotificationEmail;
+import com.sundar.reditclone.model.Post;
+import com.sundar.reditclone.model.User;
+import com.sundar.reditclone.repository.CommentRepository;
+import com.sundar.reditclone.repository.PostRepository;
+import com.sundar.reditclone.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+@AllArgsConstructor
+public class CommentService {
+    private static final String POST_URL = "";
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final AuthService authService;
+    private final CommentMapper commentMapper;
+    private final CommentRepository commentRepository;
+    private final MailContentBuilder mailContentBuilder;
+    private final MailService mailService;
+
+    public void save(CommentsDto commentsDto) {
+        Post post = postRepository.findById(commentsDto.getPostId())
+                .orElseThrow(() -> new PostNotFoundException(commentsDto.getPostId().toString()));
+        Comment comment = commentMapper.map(commentsDto, post, authService.getCurrentUser());
+        commentRepository.save(comment);
+
+        String message = mailContentBuilder.build(post.getUser().getUsername() + " posted a comment on your post." + POST_URL);
+        sendCommentNotification(message, post.getUser());
+    }
+
+    private void sendCommentNotification(String message, User user) {
+        mailService.sendMail(new NotificationEmail(user.getUsername() + " Commented on your post", user.getEmail(), message));
+    }
+
+    public List<CommentsDto> getAllCommentsForPost(Long postId) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new PostNotFoundException(postId.toString()));
+        return commentRepository.findByPost(post)
+                .stream()
+                .map(commentMapper::mapToDto).collect(toList());
+    }
+
+    public List<CommentsDto> getAllCommentsForUser(String userName) {
+        User user = userRepository.findByUsername(userName)
+                .orElseThrow(() -> new UsernameNotFoundException(userName));
+        return commentRepository.findAllByUser(user)
+                .stream()
+                .map(commentMapper::mapToDto)
+                .collect(toList());
+    }
+}
